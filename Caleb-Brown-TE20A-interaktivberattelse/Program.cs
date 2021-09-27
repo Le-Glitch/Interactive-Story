fe490079412cd96d5926a5525d86f4267b87daed
Glitch <caleb.samuel.brown@gmail.com>
2021-09-27 15:54:42 +0200

    first commit

---
+++ Caleb-Brown-TE20A-interaktivberattelse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caleb_Brown_TE20A_interaktivberattelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
