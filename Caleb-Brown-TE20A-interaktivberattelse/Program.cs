c532349d7b56b86adec7f02d9e7c889168eab9db
Glitch <caleb.samuel.brown@gmail.com>
2021-09-27 23:47:49 +0200

    Start method completed

--- Caleb-Brown-TE20A-interaktivberattelse/Program.cs
+++ Caleb-Brown-TE20A-interaktivberattelse/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace Caleb_Brown_TE20A_interaktivberattelse
+class Program
 {
-    class Program
+
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        string answer = "";
+
+
+            while(answer == "")
+            {
+                Start();
+            }
+
+            while(answer == "storyPrelude")
+            {
+                prelude();
+            }
+
+        
+        
+    void Start()
+    {
+        Console.WriteLine("Sleeping Simulator 2021");
+        Console.WriteLine();
+        Console.WriteLine("If no options are given press enter to continue");
+        Console.ReadLine();
+        answer = "storyPrelude";
     }
+
+    void prelude()
+    {
+        
+    }
+
+}
 }
