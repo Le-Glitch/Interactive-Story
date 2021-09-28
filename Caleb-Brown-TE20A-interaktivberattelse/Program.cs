af7f8259bdbeb2875e3db5be445c2667e925cd29
Glitch <caleb.samuel.brown@gmail.com>
2021-09-28 14:03:26 +0200

    Completed prelude

--- Caleb-Brown-TE20A-interaktivberattelse/Program.cs
+++ Caleb-Brown-TE20A-interaktivberattelse/Program.cs
@@ -1,38 +1,40 @@
 ﻿using System;
 
-class Program
+string answer = "";
+
+    while(answer == "")
+    {
+        Start();
+    }
+
+    while(answer == "storyPrelude")
+    {
+        prelude();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+void Start()
 {
-
-    static void Main()
-    {
-        string answer = "";
-
-
-            while(answer == "")
-            {
-                Start();
-            }
-
-            while(answer == "storyPrelude")
-            {
-                prelude();
-            }
-
-        
-        
-    void Start()
-    {
-        Console.WriteLine("Sleeping Simulator 2021");
-        Console.WriteLine();
-        Console.WriteLine("If no options are given press enter to continue");
-        Console.ReadLine();
-        answer = "storyPrelude";
-    }
-
-    void prelude()
-    {
-        
-    }
-
+    Console.WriteLine("Sleeping Simulator 2021");
+    Console.WriteLine();
+    Console.WriteLine("If No Options Are Given Press Enter to Continue");
+    Console.ReadLine();
+    answer = "storyPrelude";
 }
+
+void prelude()
+{
+    Console.WriteLine("On your way home from work you feel a bit tired.");
+    Console.WriteLine("Maybe you should sleep once you get home.");
+    Console.WriteLine("But you also have a lot to do");
+    Console.WriteLine("You'll decide when you get home, for now you just need to get there");
+    Console.ReadLine();
 }
+
+
