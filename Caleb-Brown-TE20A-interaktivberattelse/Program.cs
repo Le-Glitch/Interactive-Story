1b872ac550e28a6496f2572d21f695dfd8a397d4
Glitch <caleb.samuel.brown@gmail.com>
2021-09-28 23:43:11 +0200

    Completed all storylines

--- Caleb-Brown-TE20A-interaktivberattelse/Program.cs
+++ Caleb-Brown-TE20A-interaktivberattelse/Program.cs
@@ -4,7 +4,7 @@ string answer = "";
 
     if(answer == "")
     {
-        Intro();
+        intro();
     }
 
     if(answer == "start")
@@ -13,7 +13,67 @@ string answer = "";
         storyStart();
     }
 
- 
+
+
+    if(answer == "sleep1")
+    {
+        Console.Clear();
+        noCoffee();
+    }
+
+    if(answer == "sleep2")
+    {
+        Console.Clear();
+        noBasement();
+    }
+
+    if(answer == "sleep3")
+    {
+        Console.Clear();
+        lightsOn();
+    }
+
+
+    if(answer == "basement")
+    {
+        basement();
+    }
+
+    if(answer == "run")
+    {
+        Console.Clear();
+        run();
+    }
+
+    if(answer == "stay")
+    {
+        Console.Clear();
+        stay();
+    }
+
+    if(answer == "help")
+    {
+        Console.Clear();
+        help();
+    }
+
+    if(answer == "preserved")
+    {
+        Console.Clear();
+        preserved();
+    }
+
+    if(answer == "destroy")
+    {
+        Console.Clear();
+        destroy();
+    }
+
+    if(answer == "drink")
+    {
+        Console.Clear();
+        coffee();
+    }
     
 
 
@@ -25,7 +85,7 @@ string answer = "";
     
     
 
-void Intro()
+void intro()
 {
     Console.ForegroundColor = ConsoleColor.Red;
 
@@ -73,12 +133,6 @@ void storyStart()
     }
 }
 
-   if(answer == "sleep1")
-    {
-        Console.Clear();
-        noCoffee();
-    }
-
 
 void noCoffee()
 {
@@ -106,11 +160,6 @@ void noCoffee()
     }
 }
 
-    if(answer == "sleep2")
-    {
-        Console.Clear();
-        noBasement();
-    }
 
 void noBasement()
 {
@@ -120,31 +169,21 @@ void noBasement()
     Console.WriteLine();
     Console.WriteLine("You start getting anxious that something is going to happen but you don't know why");
     Console.WriteLine("Still you get into bed though you forgot to turn your light off before you lay down");
-    Console.WriteLine("Do you turn them off or continue laying in bed and go to sleep?");
+    Console.WriteLine("Do you turn them off or keep them on and go to sleep?");
     Console.WriteLine();
     
-    while(answer != "lightsoff" && answer != "sleep3")
+    while(answer != "lightsoff" && answer != "lightson")
     {
         Console.ForegroundColor = ConsoleColor.Green;
 
-        Console.WriteLine("Enter \"lightsoff\" or \"sleep\"");
+        Console.WriteLine("Enter \"lightsoff\" or \"lightson\"");
         answer = Console.ReadLine().ToLower();
-
-        if(answer == "sleep")
-        {
-            answer = "sleep3";
-        }        
     }
 }
 
-    if(answer == "sleep3")
-    {
-        Console.Clear();
-        sleepEnding();
-    }
     
 
-void sleepEnding()
+void lightsOn()
 {
     Console.ForegroundColor = ConsoleColor.Blue;
 
@@ -156,4 +195,101 @@ void sleepEnding()
 }
 
 
+void basement()
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
 
+    Console.WriteLine("You decide that checking out the basement is the best course of action");
+    Console.WriteLine("Slowly you open the door and walk down the creaking steps and see an ominous light coming from the corner of the room");
+    Console.WriteLine("Once you're fully down in the basement you see an otherworldly portal type thing");
+    Console.WriteLine();
+    Console.WriteLine("You go to inspect the portal when suddenly an arm of what is presumably an alien drags you through it");
+    Console.WriteLine("When you're through the portal you start being transported somewhere unknown to you but you see an opportunity to escape");
+    Console.WriteLine("Do you make a run for it or stay and face what awaits you?");
+    Console.WriteLine();
+
+    while(answer != "run" && answer != "stay")
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine("Enter \"run\" or \"stay\"");
+        answer = Console.ReadLine().ToLower();
+    }
+}
+
+void run()
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine("You decide to take the opportunity and run for the portal");
+    Console.WriteLine("Running towards it you can hear your heart racing and you're certain that you'll make it");
+    Console.WriteLine("You're nearly there, just a few more steps until freedom");
+    Console.WriteLine("Just as you think you've made it you hear some sort of laser weapon fire and then you feel it hit you");
+    Console.WriteLine("You fall down, gasping for air but it's too late, you just lay there until your inevitable death.");
+    Console.ReadLine();
+}
+
+void stay()
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine("Instead of ceasing the opportunity to run for the portal you decide to stay and see what awaits you");
+    Console.WriteLine();
+    Console.WriteLine("After a while you arrive to a door which you subsequently get forced through");
+    Console.WriteLine("In the room you see what is presumably the leader of this species sitting in a chair across a table");
+    Console.WriteLine("There is an empty chair on your side of the table and you're asked to sit in it so you obey");
+    Console.WriteLine("The aliens explain to you their plans to conquer earth and humanity and the reason they saved you");
+    Console.WriteLine("They want to keep you alive to preserve a part of the human race as a sort of historical artifact");
+    Console.WriteLine("You are given two options");
+    Console.WriteLine("Be cryofrozen so that you'll be preserved");
+    Console.WriteLine("Help them by giving them information about earth and humans and you'll be able to live out a good life");
+    Console.WriteLine();
+    Console.WriteLine("You have formulated your own plan to hijack their spacecraft and destroy their race though which adds another alternative");
+    Console.WriteLine();
+
+    if(answer != "help" && answer != "preserved" && answer != "destroy")
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine("Enter \"help\" or \"preserved\" or \"destroy\"");
+        answer = Console.ReadLine().ToLower();
+    }
+}
+
+void help()
+{
+    Console.WriteLine("You decide to help the aliens destroy earth and humanity to live out a better life yourself in this alien society");
+    Console.WriteLine("This was an elaborate ruse by the aliens though and once they're done wiping out humanity they put you in cryofreeze");
+    Console.WriteLine("The last thing you see is the ice obscuring your vision of the outside and even though not dead you'll never wake up again");
+}
+
+void preserved()
+{
+    Console.WriteLine("The aliens admire your loyalty to your species and decide that you'd be better off assimilating into their society");
+    Console.WriteLine("Even though your entire species was wiped out you live out the rest of your days surrouned by creatures you learnt to love");
+    Console.WriteLine("When your time eventually came you once again volunteeringly let them preserve you to keep the history of your people alive");
+}
+
+void destroy()
+{
+    Console.WriteLine("Unlike the opportunity you had earlier to escape you now take this chance to seize control of the ship");
+    Console.WriteLine("After fighting off the guards you have control over everything and start preparing to destroy the aliens planet");
+    Console.WriteLine();
+    Console.WriteLine("You do this and return to earth as a hero");
+    Console.WriteLine("Though before this day no one knew that aliens existed and therefore once you stepped out of the ship you instantly got shot");
+}
+
+
+void coffee()
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine("After having a cup of coffee you work on your project and finish it after a few hours");
+    Console.WriteLine("You then try to sleep and even though it takes a while due to the caffeine you eventually manage");
+    Console.WriteLine();
+    Console.WriteLine("The next day when you go into work and present the project you know that you're about to get shouted at because the presentation is horrible");
+    Console.WriteLine("Due to you completing the project so late at night your brain wasn't functioning well and that worsened your work");
+    Console.WriteLine("This bad presentation resulted in your company losing it's biggest client and due to that you got fired");
+    Console.WriteLine("Once you lost your job you could no longer pay rent and got evicted shortly after");
+    Console.WriteLine("Now you're homeless and can't get any work in the one field you're qualified to work in because of the bad reputation you have");
+}
